500c470452458595d8ee3bca823c48e86315c711
anggutierrez <gutierrezangel240@gmail.com>
2021-12-02 10:14:35 -0800

    Created all post functionality

--- src/main/java/com/group14/project3/PostService.java
+++ src/main/java/com/group14/project3/PostService.java
@@ -1,3 +1,43 @@
+package com.group14.project3;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.group14.project3.repository.postRepo.Post;
+import com.group14.project3.repository.postRepo.PostRepo;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostService {
-    
+    private final PostRepo postRepo;
+
+    @Autowired
+    public PostService(PostRepo postRepo) {
+        this.postRepo = postRepo;
+    }
+
+    public Post findByPostTitle(String title) {
+        return postRepo.findByTitle(title);
+    }
+
+    public List<Post> getPosts() {
+        return postRepo.findAll();
+    }
+
+    public Post saveOrUpdatePost(Post post) {
+        return postRepo.save(post);
+    }
+
+    public void deletePost(String id) {
+        postRepo.deleteById(id);
+    }
+
+
 }
